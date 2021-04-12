098de5d36af44c5eef73e52561a067f4f3497a52
Dominik Mocher <dominik.mocher@a-sit.at>
2021-04-12 15:35:55 +0200

    Fix local import

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     ],
     dependencies: [
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "../base45-swift/"),
-        .package(path: "../CBORSwift/"),
+        //.package(path: "../base45-swift/"),
+        //.package(path: "../CBORSwift/"),
         .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.7.0"),
         .package(name: "Gzip", url: "https://github.com/1024jp/GzipSwift", from: "5.1.1"),
         .package(url: "https://github.com/unrelentingtech/SwiftCBOR", from: "0.4.3"),
@@ -23,11 +23,12 @@ let package = Package(
     targets: [
         .target(
             name: "ValidationCore",
-            dependencies: ["base45-swift",
+            dependencies: [//"base45-swift",
                            .product(name: "CocoaLumberjackSwift", package: "CocoaLumberjack"),
                            "Gzip",
                            "SwiftCBOR",
-                           "CBORSwift"]),
+                           //"CBORSwift"
+                           ]),
         .testTarget(
             name: "ValidationCoreTests",
             dependencies: ["ValidationCore"]),
