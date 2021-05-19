0a9b928027fd5a5a8dcc44dcd89f70c83e4bcd6f
Dominik Mocher <dominik.mocher@a-sit.at>
2021-05-19 09:08:30 +0200

    Check if health cert follows scheme rules

---
+++ Sources/ValidationCore/DGCValueSets.swift
@@ -0,0 +1,185 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dominik Mocher on 19.05.21.
+//
+
+import Foundation
+
+public enum DiseaseAgentTargeted : String, Codable {
+    case COVID19 = "840539006"
+    
+    public func humanReadable() -> String {
+        switch self {
+        case .COVID19:
+            return "COVID-19"
+        }
+    }
+}
+
+public enum TestManufacturer : String, Codable {
+    case manufacturer_1232 = "1232"
+    case manufacturer_1304 = "1304"
+    case manufacturer_1065 = "1065"
+    case manufacturer_1331 = "1331"
+    case manufacturer_1484 = "1484"
+    case manufacturer_1242 = "1242"
+    case manufacturer_1223 = "1223"
+    case manufacturer_1173 = "1173"
+    case manufacturer_1244 = "1244"
+    case manufacturer_1360 = "1360"
+    case manufacturer_1363 = "1363"
+    case manufacturer_1767 = "1767"
+    case manufacturer_1333 = "1333"
+    case manufacturer_1268 = "1268"
+    case manufacturer_1180 = "1180"
+    case manufacturer_1481 = "1481"
+    case manufacturer_1162 = "1162"
+    case manufacturer_1271 = "1271"
+    case manufacturer_1341 = "1341"
+    case manufacturer_1097 = "1097"
+    case manufacturer_1489 = "1489"
+    case manufacturer_344 = "344"
+    case manufacturer_345 = "345"
+    case manufacturer_1218 = "1218"
+    case manufacturer_1278 = "1278"
+    case manufacturer_1343 = "1343"
+    
+    public func humanReadable() -> String {
+        switch self {
+        case .manufacturer_1232: return "Abbott Rapid Diagnostics, Panbio COVID-19 Ag Test"
+        case .manufacturer_1304: return "AMEDA Labordiagnostik GmbH, AMP Rapid Test SARS-CoV-2 Ag"
+        case .manufacturer_1065: return "Becton Dickinson, Veritor System Rapid Detection of SARS-CoV-2"
+        case .manufacturer_1331: return "Beijing Lepu Medical Technology Co., Ltd, SARS-CoV-2 Antigen Rapid Test Kit"
+        case .manufacturer_1484: return "Beijing Wantai Biological Pharmacy Enterprise Co., Ltd, Wantai SARS-CoV-2 Ag Rapid Test (FIA)"
+        case .manufacturer_1242: return "Bionote, Inc, NowCheck COVID-19 Ag Test"
+        case .manufacturer_1223: return "BIOSYNEX SWISS SA, BIOSYNEX COVID-19 Ag BSS"
+        case .manufacturer_1173: return "CerTest Biotec, S.L., CerTest SARS-CoV-2 Card test"
+        case .manufacturer_1244: return "GenBody, Inc, Genbody COVID-19 Ag Test"
+        case .manufacturer_1360: return "Guangdong Wesail Biotech Co., Ltd, COVID-19 Ag Test Kit"
+        case .manufacturer_1363: return "Hangzhou Clongene Biotech Co., Ltd, Covid-19 Antigen Rapid Test Kit"
+        case .manufacturer_1767: return "Healgen Scientific Limited Liability Company, Coronavirus Ag Rapid Test Cassette"
+        case .manufacturer_1333: return "Joinstar Biomedical Technology Co., Ltd, COVID-19 Rapid Antigen Test (Colloidal Gold)"
+        case .manufacturer_1268: return "LumiraDX UK Ltd, LumiraDx SARS-CoV-2 Ag Test"
+        case .manufacturer_1180: return "MEDsan GmbH, MEDsan SARS-CoV-2 Antigen Rapid Test"
+        case .manufacturer_1481: return "MP Biomedicals Germany GmbH, Rapid SARS-CoV-2 Antigen Test Card"
+        case .manufacturer_1162: return "Nal von minden GmbH, NADAL COVID-19 Ag Test"
+        case .manufacturer_1271: return "Precision Biosensor, Inc, Exdia COVID-19 Ag"
+        case .manufacturer_1341: return "Qingdao Hightop Biotech Co., Ltd, SARS-CoV-2 Antigen Rapid Test (Immunochromatography)"
+        case .manufacturer_1097: return "Quidel Corporation, Sofia SARS Antigen FIA"
+        case .manufacturer_1489: return "Safecare Biotech (Hangzhou) Co. Ltd, COVID-19 Antigen Rapid Test Kit (Swab)"
+        case .manufacturer_344: return "SD BIOSENSOR Inc, STANDARD F COVID-19 Ag FIA"
+        case .manufacturer_345: return "SD BIOSENSOR Inc, STANDARD Q COVID-19 Ag Test"
+        case .manufacturer_1218: return "Siemens Healthineers, CLINITEST Rapid Covid-19 Antigen Test"
+        case .manufacturer_1278: return "Xiamen Boson Biotech Co. Ltd, Rapid SARS-CoV-2 Antigen Test Card"
+        case .manufacturer_1343: return "Zhejiang Orient Gene Biotech, Coronavirus Ag Rapid Test Cassette (Swab)"
+        }
+    }
+}
+
+public enum TestResult : String, Codable {
+    case result_260415000 = "260415000"
+    case result_26037300 = "26037300"
+    
+    public func humanReadable() -> String {
+        switch self {
+        case .result_260415000: return "Not detected"
+        case .result_26037300: return "Detected"
+        }
+    }
+}
+
+
+public enum TestType : String, Codable {
+    case LP64644 = "LP6464-4"
+    case LP2171983 = "LP217198-3"
+    
+    public func humanReadable() -> String {
+        switch self {
+        case .LP64644: return "Nucleic acid amplification with probe detection"
+        case .LP2171983: return "Rapid immunoassay"
+        }
+    }
+}
+
+public enum VaccineManufacturer : String, Codable {
+    case ORG100001699 = "ORG-100001699"
+    case ORG100030215 = "ORG-100030215"
+    case ORG100001417 = "ORG-100001417"
+    case ORG100031184 = "ORG-100031184"
+    case ORG100006270 = "ORG-100006270"
+    case ORG100013793 = "ORG-100013793"
+    case ORG100020693 = "ORG-100020693"
+    case ORG100010771 = "ORG-100010771"
+    case ORG100024420 = "ORG-100024420"
+    case ORG100032020 = "ORG-100032020"
+    case GamaleyaResearchInstitute = "Gamaleya-Research-Institute"
+    case VectorInstitute = "Vector-Institute"
+    case SinovacBiotech = "Sinovac-Biotech"
+    case BharatBiotech = "Bharat-Biotech"
+    
+    public func humanReadable() -> String {
+        switch self {
+        case .ORG100001699: return "AstraZeneca AB"
+        case .ORG100030215: return "Biontech Manufacturing GmbH"
+        case .ORG100001417: return "Janssen-Cilag International"
+        case .ORG100031184: return "Moderna Biotech Spain S.L."
+        case .ORG100006270: return "Curevac AG"
+        case .ORG100013793: return "CanSino Biologics"
+        case .ORG100020693: return "China Sinopharm International Corp. - Beijing location"
+        case .ORG100010771: return "Sinopharm Weiqida Europe Pharmaceutical s.r.o. - Prague location"
+        case .ORG100024420: return "Sinopharm Zhijun (Shenzhen) Pharmaceutical Co. Ltd. - Shenzhen location"
+        case .ORG100032020: return "Novavax CZ AS"
+        case .GamaleyaResearchInstitute: return "Gamaleya Research Institute"
+        case .VectorInstitute: return "Vector Institute"
+        case .SinovacBiotech: return "Sinovac Biotech"
+        case .BharatBiotech: return "Bharat Biotech"
+        }}
+}
+
+public enum VaccineMedicinialProduct : String, Codable {
+    case EU1201528 =  "EU/1/20/1528"
+    case EU1201507 = "EU/1/20/1507"
+    case EU1211529 = "EU/1/21/1529"
+    case EU1201525 = "EU/1/20/1525"
+    case CVnCoV = "CVnCoV"
+    case SputnikV = "Sputnik-V"
+    case Convidecia = "Convidecia"
+    case EpiVacCorona = "EpiVacCorona"
+    case BBIBPCorV = "BBIBP-CorV"
+    case InactivatedSARSCoV2VeroCell = "Inactivated-SARS-CoV-2-Vero-Cell"
+    case CoronaVac = "CoronaVac"
+    case Covaxin = "Covaxin"
+    
+    public func humanReadable() -> String {
+        switch self {
+        case .EU1201528: return "Comirnaty"
+        case .EU1201507: return "COVID-19 Vaccine Moderna"
+        case .EU1211529: return "Vaxzevria"
+        case .EU1201525: return "COVID-19 Vaccine Janssen"
+        case .CVnCoV: return "CVnCoV"
+        case .SputnikV: return "Sputnik-V"
+        case .Convidecia: return "Convidecia"
+        case .EpiVacCorona: return "EpiVacCorona"
+        case .BBIBPCorV: return "BBIBP-CorV"
+        case .InactivatedSARSCoV2VeroCell: return "Inactivated SARS-CoV-2 (Vero Cell)"
+        case .CoronaVac: return "CoronaVac"
+        case .Covaxin: return "Covaxin (also known as BBV152 A, B, C)"
+        }
+    }
+}
+
+public enum VaccineProphylaxis : String, Codable {
+    case vaccine_1119349007 = "1119349007"
+    case vaccine_1119305005 = "1119305005"
+    case vaccine_J07BX03 = "J07BX03"
+    
+    public func humanReadable() -> String {
+        switch self {
+        case .vaccine_1119349007: return "SARS-CoV-2 mRNA vaccine"
+        case .vaccine_1119305005: return "SARS-CoV-2 antigen vaccine"
+        case .vaccine_J07BX03: return "covid-19 vaccines"
+        }
+    }
+}
--- Sources/ValidationCore/EuHealthCert.swift
+++ Sources/ValidationCore/EuHealthCert.swift
@@ -8,6 +8,8 @@
 import Foundation
 import SwiftCBOR
 
+fileprivate let SCHEMA_LENGTH_LIMIT = 50
+
 public struct EuHealthCert : Codable {
     public let person: Person
     public let dateOfBirth : String
@@ -27,8 +29,8 @@ public struct EuHealthCert : Codable {
                 return .test
             }
         }
-   }
-
+    }
+    
     private enum CodingKeys : String, CodingKey {
         case person = "nam"
         case dateOfBirth = "dob"
@@ -37,12 +39,20 @@ public struct EuHealthCert : Codable {
         case tests = "t"
         case version = "ver"
     }
- 
-   public init(from decoder: Decoder) throws {
+    
+    public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.person = try container.decode(Person.self, forKey: .person)
-        self.version = try container.decode(String.self, forKey: .version)
-        self.dateOfBirth = try container.decode(String.self, forKey: .dateOfBirth)
+        let version = try container.decode(String.self, forKey: .version)
+        guard version.range(of: "^\\d+.\\d+.\\d+$", options: .regularExpression) != nil else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.version = version
+        let dateOfBirth = try container.decode(String.self, forKey: .dateOfBirth)
+        guard dateOfBirth.range(of: "(19|20)\\d{2}-\\d{2}-\\d{2}", options: .regularExpression) != nil else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.dateOfBirth = dateOfBirth
         self.vaccinations = try? container.decode([Vaccination].self, forKey: .vaccinations)
         self.tests = try? container.decode([Test].self, forKey: .tests)
         self.recovery = try? container.decode([Recovery].self, forKey: .recovery)
@@ -54,20 +64,40 @@ public struct Person : Codable {
     public let standardizedGivenName: String?
     public let familyName: String?
     public let standardizedFamilyName: String
-
+    
     private enum CodingKeys : String, CodingKey {
         case givenName = "gn"
         case standardizedGivenName = "gnt"
         case familyName = "fn"
         case standardizedFamilyName = "fnt"
     }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.givenName = try? container.decode(String.self, forKey: .givenName)
+        if let givenName = givenName, givenName.count > SCHEMA_LENGTH_LIMIT {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.standardizedGivenName = try? container.decode(String.self, forKey: .standardizedGivenName)
+        if let standardizedGivenName = standardizedGivenName, standardizedGivenName.range(of: "^[A-Z<]*$", options: .regularExpression) == nil {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.familyName = try? container.decode(String.self, forKey: .familyName)
+        if let familyName = familyName, familyName.count > SCHEMA_LENGTH_LIMIT {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.standardizedFamilyName = try container.decode(String.self, forKey: .standardizedFamilyName)
+        if standardizedFamilyName.range(of: "^[A-Z<]*$", options: .regularExpression) == nil {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+    }
 }
 
 public struct Vaccination : Codable {
-    public let disease: String
-    public let vaccine: String
-    public let medicinialProduct: String
-    public let marketingAuthorizationHolder: String
+    public let disease: DiseaseAgentTargeted //disease-agent-targeted
+    public let vaccine: VaccineProphylaxis //vaccine-prophylaxis
+    public let medicinialProduct: VaccineMedicinialProduct //vaccine-medicinal-product
+    public let marketingAuthorizationHolder: VaccineManufacturer //vaccine-mah-manf
     public let doseNumber: UInt64
     public let totalDoses: UInt64
     public let vaccinationDate: String
@@ -76,32 +106,67 @@ public struct Vaccination : Codable {
     public let certificateIdentifier: String
     
     private enum CodingKeys : String, CodingKey {
-           case disease = "tg"
-           case vaccine = "vp"
-           case medicinialProduct = "mp"
-           case marketingAuthorizationHolder = "ma"
-           case doseNumber = "dn"
-           case totalDoses = "sd"
-           case vaccinationDate = "dt"
-           case country = "co"
+        case disease = "tg"
+        case vaccine = "vp"
+        case medicinialProduct = "mp"
+        case marketingAuthorizationHolder = "ma"
+        case doseNumber = "dn"
+        case totalDoses = "sd"
+        case vaccinationDate = "dt"
+        case country = "co"
         case certificateIssuer = "is"
         case certificateIdentifier = "ci"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.disease = try container.decode(DiseaseAgentTargeted.self, forKey: .disease)
+        self.vaccine = try container.decode(VaccineProphylaxis.self, forKey: .vaccine)
+        self.medicinialProduct = try container.decode(VaccineMedicinialProduct.self, forKey: .medicinialProduct)
+        self.marketingAuthorizationHolder = try container.decode(VaccineManufacturer.self, forKey: .marketingAuthorizationHolder)
+        self.doseNumber = try container.decode(UInt64.self, forKey: .doseNumber)
+        guard 1..<10 ~= doseNumber else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.totalDoses = try container.decode(UInt64.self, forKey: .totalDoses)
+        guard 1..<10 ~= totalDoses else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        let vaccinationDate = try container.decode(String.self, forKey: .vaccinationDate)
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = .withFullDate
+        guard formatter.date(from: vaccinationDate) != nil else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.vaccinationDate = vaccinationDate
+        self.country = try container.decode(String.self, forKey: .country)
+        guard country.range(of: "[A-Z]{1,10}", options: .regularExpression) != nil else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.certificateIssuer = try container.decode(String.self, forKey: .certificateIssuer)
+        guard certificateIssuer.count <= SCHEMA_LENGTH_LIMIT else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.certificateIdentifier = try container.decode(String.self, forKey: .certificateIdentifier)
+        guard certificateIdentifier.count <= SCHEMA_LENGTH_LIMIT else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
     }
 }
 
 public struct Test : Codable {
     public let disease: String
-    public let type: String
+    public let type: TestType
     public let testName: String?
-    public let manufacturer: String?
+    public let manufacturer: TestManufacturer?
     public let timestampSample: String
     public let timestampResult : String?
-    public let result: String
+    public let result: TestResult
     public let testCenter: String
     public let country: String
     public let certificateIssuer: String
     public let certificateIdentifier: String
-
+    
     private enum CodingKeys : String, CodingKey {
         case disease = "tg"
         case type = "tt"
@@ -115,10 +180,48 @@ public struct Test : Codable {
         case certificateIssuer = "is"
         case certificateIdentifier = "ci"
     }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.disease = try container.decode(String.self, forKey: .disease)
+        self.type = try container.decode(TestType.self, forKey: .type)
+        self.testName = try? container.decode(String.self, forKey: .testName)
+        
+        self.manufacturer = try? container.decode(TestManufacturer.self, forKey: .manufacturer)
+        let timestampSample = try container.decode(String.self, forKey: .timestampSample)
+        guard ISO8601DateFormatter().date(from: timestampSample) != nil else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.timestampSample = timestampSample
+        let timestampResult = try? container.decode(String.self, forKey: .timestampResult )
+        if let timestampResult = timestampResult, ISO8601DateFormatter().date(from: timestampResult) == nil {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.timestampResult = timestampResult
+        self.result = try container.decode(TestResult.self, forKey: .result)
+        let testCenter = try container.decode(String.self, forKey: .testCenter)
+        guard testCenter.count <= SCHEMA_LENGTH_LIMIT else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.testCenter = testCenter
+        self.country = try container.decode(String.self, forKey: .country)
+        guard country.range(of: "[A-Z]{1,10}", options: .regularExpression) != nil else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.certificateIssuer = try container.decode(String.self, forKey: .certificateIssuer)
+        guard certificateIssuer.count <= SCHEMA_LENGTH_LIMIT else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.certificateIdentifier = try container.decode(String.self, forKey: .certificateIdentifier)
+        guard certificateIdentifier.count <= SCHEMA_LENGTH_LIMIT else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+    }
+    
 }
 
 public struct Recovery : Codable {
-    public let disease: String
+    public let disease: DiseaseAgentTargeted
     public let dateFirstPositiveTest: String
     public let countryOfTest: String
     public let certificateIssuer: String
@@ -135,5 +238,36 @@ public struct Recovery : Codable {
         case validUntil = "du"
         case certificateIdentifier = "ci"
     }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.disease = try container.decode(DiseaseAgentTargeted.self, forKey: .disease)
+        let dateFirstPositiveTest = try container.decode(String.self, forKey: .dateFirstPositiveTest)
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = .withFullDate
+        guard formatter.date(from: dateFirstPositiveTest) != nil else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.dateFirstPositiveTest = dateFirstPositiveTest
+        self.countryOfTest = try container.decode(String.self, forKey: .countryOfTest)
+        guard countryOfTest.range(of: "[A-Z]{1,10}", options: .regularExpression) != nil else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.validFrom = try container.decode(String.self, forKey: .validFrom)
+        guard formatter.date(from: validFrom) != nil else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.validUntil = try container.decode(String.self, forKey: .validUntil)
+        guard formatter.date(from: validUntil) != nil else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.certificateIssuer = try container.decode(String.self, forKey: .certificateIssuer)
+        guard certificateIssuer.count <= SCHEMA_LENGTH_LIMIT else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+        self.certificateIdentifier = try container.decode(String.self, forKey: .certificateIdentifier)
+        guard certificateIdentifier.count <= SCHEMA_LENGTH_LIMIT else {
+            throw ValidationError.CBOR_DESERIALIZATION_FAILED
+        }
+    }
 }
-
