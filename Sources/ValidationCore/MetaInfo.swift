90a5f056777e8f3179ca28a95717a654d747cec6
Dominik Mocher <dominik.mocher@a-sit.at>
2021-06-21 09:27:33 +0200

    Include CWT nbf in result

--- Sources/ValidationCore/MetaInfo.swift
+++ Sources/ValidationCore/MetaInfo.swift
@@ -8,13 +8,15 @@
 import Foundation
 
 public struct MetaInfo : Codable {
-    let expirationTime : String?
-    let issuedAt : String?
-    let issuer : String?
+    public let expirationTime : String?
+    public let issuedAt : String?
+    public let issuer : String?
+    public let notBefore : String?
     
     init(from cwt: CWT) {
         expirationTime = cwt.exp?.toIso8601DateString()
         issuedAt = cwt.iat?.toIso8601DateString()
         issuer = cwt.iss
+        notBefore = cwt.nbf?.toIso8601DateString()
     }
 }
